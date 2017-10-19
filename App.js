7872838e37addcb2077e70eccd1b8f11088097c6
Danny Gilbert <danny_gilbert@icloud.com>
2017-10-19 11:00:45 -0500

    Initial Commit

---
+++ App.js
@@ -0,0 +1,128 @@
+import React, { Component } from 'react';
+import { 
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TextInput,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+  Image
+} from 'react-native';
+import {
+  send,
+  subscribe
+} from 'react-native-training-chat-server';
+import ReversedFlatList from 'react-native-reversed-flat-list';
+import Header from './src/Header';
+
+const NAME = 'Creator'
+const CHANNEL = 'React Native Chat'
+const AVATAR = 'https://pbs.twimg.com/profile_images/806501058679816192/ZHFWIF-z_400x400.jpg';
+
+export default class App extends Component {
+  state = {
+    typing: "",
+    messages: [],
+  };
+
+  componentDidMount() {
+    subscribe(CHANNEL, messages => {
+      this.setState({messages});
+    });
+  }
+
+  async sendMessage() {
+    await send({
+      channel: CHANNEL,
+      sender: NAME,
+      avatar: AVATAR,
+      message: this.state.typing
+    });
+
+    this.setState({
+      typing: '',
+    });
+  }
+
+  renderItem({item}) {
+    return (
+      <View style={styles.row}>
+        <Image style={styles.avatar} source={{uri: item.avatar}}/>
+        <View style={styles.rowText}>
+          <Text style={styles.sender}>{item.sender}</Text>
+          <Text style={styles.message}>{item.message}</Text>
+        </View>
+      </View>
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header title={CHANNEL}/>
+        <ReversedFlatList data={this.state.messages} renderItem={this.renderItem} />
+        <KeyboardAvoidingView behavior="padding">
+        <View style={styles.footer}>
+          <TextInput
+            value={this.state.typing}
+            onChangeText={text => this.setState({typing: text})}
+            style={styles.input}
+            underlineColorAndroid="transparent"
+            placeholder="Type something nice"
+            onChangeText={text => this.setState({typing: text})}
+          />
+          <TouchableOpacity onPress={this.sendMessage.bind(this)}>
+            <Text style={styles.send}>Send</Text>
+          </TouchableOpacity>
+        </View>
+        </KeyboardAvoidingView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  row: {
+    padding: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee',
+  },
+  message: {
+    fontSize: 18,
+  },
+  sender: {
+    fontWeight: 'bold',
+    paddingRight: 10,
+  },
+  footer: {
+    flexDirection: 'row',
+    backgroundColor: '#eee',
+  },
+  input: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    fontSize: 18,
+    flex: 1,
+  },
+  send: {
+    alignSelf: 'center',
+    color: 'lightseagreen',
+    fontSize: 16,
+    fontWeight: 'bold',
+    padding: 20,
+  },
+  avatar: {
+    borderRadius: 20,
+    width: 40,
+    height: 40,
+    marginRight: 10,
+  },
+  rowText: {
+    flex: 1,
+  },
+});
